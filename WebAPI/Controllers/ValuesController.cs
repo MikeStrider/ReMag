72447ba61a884a39e27eb1bd11b72ce2cbf1581c
Michael Strong <34251532+MikeStrider@users.noreply.github.com>
2020-12-28 23:22:38 -0500

    update 6

---
+++ WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(string id, [FromBody]string bodythings)
+        {
+            return bodythings;
+        }
+
+        // POST api/values
+        public string Post(HttpRequestMessage value)
+        {
+            string x = Request.Content.ReadAsStringAsync().Result;
+            return x;
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
